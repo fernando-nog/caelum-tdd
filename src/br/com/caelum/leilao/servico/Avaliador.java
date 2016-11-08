47591779d3c78a5f239b8de46bb84acf45208b45
Fernando Nogueira <fernando.nogueira@mv.com.br>
2016-11-08 13:33:25 -0300

    Exercícios 2º aula

--- src/br/com/caelum/leilao/servico/Avaliador.java
+++ src/br/com/caelum/leilao/servico/Avaliador.java
@@ -1,40 +1,49 @@
 package br.com.caelum.leilao.servico;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 import br.com.caelum.leilao.dominio.Lance;
 import br.com.caelum.leilao.dominio.Leilao;
 
 public class Avaliador {
 
-	private double maiorLance = Double.NEGATIVE_INFINITY;
-	private double menorLance = Double.POSITIVE_INFINITY;
-	private double media = 0; 
-	
-	public void avalia(Leilao leilao){
-		double total = 0;
-		for (Lance lance : leilao.getLances()) {
-			if (lance.getValor() > maiorLance) {
-				maiorLance = lance.getValor();
-			}
-			if (lance.getValor() < menorLance ) {
-				menorLance = lance.getValor();
-			}
-			total += lance.getValor();
-		}
-		int totalLances = leilao.getLances().size();
-		
-		if(totalLances > 0)
-			media = total/totalLances;
-	}
-	
-	public double getMaiorLance() {
-		return maiorLance;
-	}
-	
-	public double getMenorLance() {
-		return menorLance;
-	}
-	
-	public double getMedia(){
-		return media;
-	}
+	private double maiorDeTodos = Double.NEGATIVE_INFINITY;
+    private double menorDeTodos = Double.POSITIVE_INFINITY;
+    private List<Lance> maiores;
+
+    public void avalia(Leilao leilao) {
+        for(Lance lance : leilao.getLances()) {
+            if(lance.getValor() > maiorDeTodos) maiorDeTodos = lance.getValor();
+            if (lance.getValor() < menorDeTodos) menorDeTodos = lance.getValor();
+        }
+
+        pegaOsMaioresNo(leilao);
+    }
+
+    private void pegaOsMaioresNo(Leilao leilao) {
+        maiores = new ArrayList<Lance>(leilao.getLances());
+        Collections.sort(maiores, new Comparator<Lance>() {
+            public int compare(Lance o1, Lance o2) {
+                if(o1.getValor() < o2.getValor()) return 1;
+                if(o1.getValor() > o2.getValor()) return -1;
+                return 0;
+            }
+        });
+        maiores = maiores.subList(0, maiores.size() > 3 ? 3 : maiores.size());
+    }
+
+    public List<Lance> getTresMaiores() {
+        return this.maiores;
+    }
+
+    public double getMaiorLance() {
+        return maiorDeTodos;
+    }
+
+    public double getMenorLance() {
+        return menorDeTodos;
+    }
 }
--- test/br/com/caelum/leilao/servico/TesteDoAvaliador.java
+++ test/br/com/caelum/leilao/servico/TesteDoAvaliador.java
@@ -54,4 +54,59 @@ public class TesteDoAvaliador {
 
 		assertEquals(200.00, avaliador.getMedia(), delta);
 	}
+	
+	@Test
+	public void deveEntenderLeilaoComUmLance(){
+		Avaliador avaliador = new Avaliador();
+		Usuario joao = new Usuario("joao");
+		Leilao leilao = new Leilao("PS3");
+		leilao.propoe(new Lance(joao, 200));
+		avaliador.avalia(leilao);
+		assertEquals(200.00, avaliador.getMaiorLance(), delta);
+		assertEquals(200.00, avaliador.getMenorLance(), delta);
+		assertEquals(200.00, avaliador.getMedia(), delta);
+	}
+	
+	@Test
+	public void deveEntenderLancesDeFormaAleatoria(){
+		Avaliador avaliador = new Avaliador();
+		Usuario joao = new Usuario("Joao");
+		Usuario maria = new Usuario("Maria");
+		Leilao leilao = new Leilao("PS3");
+		
+		leilao.propoe(new Lance(joao, 200));
+		leilao.propoe(new Lance(joao, 450));
+		leilao.propoe(new Lance(joao, 120));
+		
+		leilao.propoe(new Lance(maria, 700));
+		leilao.propoe(new Lance(maria, 630));
+		leilao.propoe(new Lance(maria, 230));
+
+		avaliador.avalia(leilao);
+		
+		assertEquals(700.00, avaliador.getMaiorLance(), delta);
+		assertEquals(120.00, avaliador.getMenorLance(), delta);
+	}
+	
+	
+	@Test
+	public void deveEntenderLancesEmOrdemDecrescente(){
+		Avaliador avaliador = new Avaliador();
+		Usuario joao = new Usuario("Joao");
+		Leilao leilao = new Leilao("PS3");
+		
+		leilao.propoe(new Lance(joao, 400));
+		leilao.propoe(new Lance(joao, 300));
+		leilao.propoe(new Lance(joao, 100));
+		
+		avaliador.avalia(leilao);
+		
+		assertEquals(400.00, avaliador.getMaiorLance(), delta);
+		assertEquals(100.00, avaliador.getMenorLance(), delta);
+	}
+	
+	//TODO impelementar seguintes testes:
+//    Um leilão com 5 lances, deve encontrar os três maiores
+//    Um leilão com 2 lances, deve devolver apenas os dois lances que encontrou
+//    Um leilão sem nenhum lance, devolve lista vazia
 }
